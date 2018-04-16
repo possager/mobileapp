ceef54cf32230e3215154df878e6122c56c807c8
possager <passager@163.com>
2018-04-16 11:15:39 +0800

    调整我的数据库存储路径，增加存储模块。初步完成部署。

---
+++ mobileapp/Push_Data2Mongo.py
@@ -0,0 +1,122 @@
+import pymongo
+import os
+import sys
+import json
+import pymongo
+
+
+
+
+def scanf_file_store_data():
+    basic_file='/Liang_Spider/spider_data_store/'
+    os.chdir(basic_file)
+
+    app_dir_root=os.listdir('.')
+
+    #进入数据app文件夹目录
+    for one_file in app_dir_root:
+        app_name_path=basic_file+one_file+'/'
+        os.chdir(app_name_path)
+
+        all_date_dir=os.listdir(app_name_path)
+
+        for one_date_dir in all_date_dir:
+            if one_date_dir=='1111-11-11':
+                continue
+
+            app_name_date=app_name_path+one_date_dir+'/'
+            all_app_data=os.listdir(app_name_date)
+
+            for one_data in all_app_data:
+                print(app_name_date+one_data)
+
+
+def scanf_file_org_and_save():
+    basic_file = '/Liang_Spider/spider_data_store/'
+    os.chdir(basic_file)
+
+    all_data=os.listdir(basic_file)
+    if len(all_data)>20:
+
+        mongoclient = pymongo.MongoClient('178.16.7.86', 27017)
+        appCOL = mongoclient['news']
+        newsdata = appCOL['news_content']
+
+
+
+        for one_file in all_data:
+            one_org_file_path=basic_file+one_file
+            with open(one_org_file_path,'r') as fl:
+                datajson=json.load(fl)
+
+            #handle reply_nodes first
+            try:
+                if datajson['publish_time']=='1111-11-11 11:11:11':
+                    try:
+                        os.remove(one_org_file_path)
+                    except Exception as e:
+                        print(e)
+                        return
+                    continue
+                parent_id=datajson['id']
+                reply_nodes = datajson['reply_nodes']
+                appname=datajson['appname']
+                app_comment_db_name=appname+'_comment'
+                comment_db = appCOL[app_comment_db_name]
+
+                for one_reply in reply_nodes:
+                    one_reply['ancestor_id'] = parent_id
+                    one_reply['news_id'] = datajson['urlmd5']
+                    one_reply['publish_user_id'] = parent_id
+                    try:
+                        result_cursor = comment_db.find(
+                            {'news_id': one_reply['news_id'], 'publish_user': one_reply['publish_user']})
+                        count = result_cursor.count()
+                        if count > 0:
+                            _id1 = result_cursor[0]['_id']
+                        else:
+                            raise Exception
+                        comment_db.update({'_id': _id1}, one_reply)
+                    except Exception as e:
+                        comment_db.insert(one_reply)
+                        pass
+                del datajson['reply_nodes']
+            except Exception as e:
+                print(e)
+
+
+            #to ensure datajson dosen't contains reply_nodes
+            try:
+                del datajson['reply_nodes']
+            except Exception as e:
+                print(e)
+
+            #handle org_data secends
+            try:
+                result_cursor_content = newsdata.find({'urlmd5': datajson['urlmd5']})
+                count = result_cursor_content.count()
+                if count > 0:
+                    _id2 = result_cursor_content[0]['_id']
+                else:
+                    raise Exception
+
+                newsdata.update({'_id': _id2}, datajson)
+            except Exception as e:
+                newsdata.insert(datajson)
+                pass
+
+            #most importantly,delete this data
+            try:
+                os.remove(one_org_file_path)
+            except Exception as e:
+                print(e)
+                #return when wrong!avoid remaining pushing the same data to Mongo!
+                return
+
+
+
+
+
+if __name__ == '__main__':
+    # scanf_file()
+    scanf_file_org_and_save()
--- mobileapp/pipelines.py
+++ mobileapp/pipelines.py
@@ -14,14 +14,15 @@ import platform
 
 basic_file = 'E:/scrapy_data/'
 if platform.system() == 'Linux':
-    basic_file = '/home/ll_spider2/spider_data_all'
+    basic_file = '/Liang_Spider/spider_data_store'
+    basic_file_org='/Liang_Spider/spider_data_org'
 
 
 
 class MobileappPipeline(object):
 
     def process_item(self, item, spider):
-        def save_data(file_path, file, full_data):  # 因为后来要用到存储的时候的文件名，先要调用里边的文件名，所以生成文件名和爬取数据结果应该分开写。
+        def save_data_store(file_path, file, full_data):  # 因为后来要用到存储的时候的文件名，先要调用里边的文件名，所以生成文件名和爬取数据结果应该分开写。
 
             publish_date=file.split(' ')[0]
             filename=full_data['appname']+'_'+full_data['urlmd5']
@@ -42,10 +43,17 @@ class MobileappPipeline(object):
                 with open(file_path_and_name, 'w+') as cmfl:
                     json.dump(full_data, cmfl)
 
+        def save_data_org(full_data):
+            filename = full_data['appname'] + '_' + full_data['urlmd5']
+            with open(basic_file_org+'/'+filename,'w+') as fl:
+                json.dump(full_data,fl)
+
 
         if isinstance(item,MobileappItem):
             item_dict=dict(item)
-            save_data(file_path=basic_file+item_dict['appname'],file=item_dict['publish_time'],full_data=item_dict)
+            save_data_store(file_path=basic_file+item_dict['appname'],file=item_dict['publish_time'],full_data=item_dict)
+            save_data_org(full_data=item_dict)
+
 
 
 
